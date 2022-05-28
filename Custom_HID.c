fba191e4c1957988e2b195853c3ae3479567a435
fyodore82 <fyodor_e@hotmail.com>
2022-05-28 11:37:22 +1000

    Add Reboot to bootloader USB command

--- Custom_HID.c
+++ Custom_HID.c
@@ -1,6 +1,7 @@
 #include "Custom_HID.h"
 #include <string.h>
 #include <stdlib.h>
+#include <xc.h>
 
 unsigned char txData[64];
 
@@ -11,7 +12,24 @@ void ProcessRxFrame(unsigned char* UsbRxData, unsigned char len)
     case 0x90: {
       txData[0] = 0x01;
       txData[1] = 0x90;
+      break;
     }
+    
+    case 0x80: {
+      // Do software reset
+      SYSKEY = 0x00000000; //write invalid key to force lock
+      SYSKEY = 0xAA996655; //write key1 to SYSKEY
+      SYSKEY = 0x556699AA; //write key2 to SYSKEY
+      // OSCCON is now unlocked
+      /* set SWRST bit to arm reset */
+      RSWRSTSET = 1;
+      /* read RSWRST register to trigger reset */
+      unsigned int dummy;
+      dummy = RSWRST;
+      /* prevent any unwanted code execution until reset occurs*/
+      while(1);
+      break;
+	  }
   }
 }
 
